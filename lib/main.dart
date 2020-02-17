5bd59040fd8fc532ce16797fc7aaf568a91b0450
mikkelsandberg <mikkel.h.sandberg@gmail.com>
2020-02-17 12:39:37 -0800

    Adding static dice

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('dice 1');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print('dice 2');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
